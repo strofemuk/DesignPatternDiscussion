fbfa160b7865b2c4bb8c248736d453d442e906da
Steve Trofemuk <steve@trofemuk.org>
2016-12-19 08:49:55 -0500

    Cleanup variable names

--- Program.cs
+++ Program.cs
@@ -30,28 +30,28 @@ namespace FluentBuilderDemo
             //object construction using the classic builder pattern.  
             //The director (shop) is used to conduct the building operation (builder) to produce an object (firstCar)
             Builder.IVehicleBuilder builder = new Builder.CarBuilder();
-            Builder.IBuildDirector shop = new Builder.BuildDirector();
-            Builder.Vehicle firstCar = shop.Construct(builder);
-            Console.WriteLine(firstCar.ToString());
+            Builder.IBuildDirector director = new Builder.BuildDirector();
+            Builder.Vehicle car1 = director.Construct(builder);
+            Console.WriteLine(car1.ToString());
 
             //let's build another one....
             Builder.IVehicleBuilder motorcycleBuilder = new Builder.MotorcycleBuilder();
-            Builder.Vehicle motorcycle3 = shop.Construct(motorcycleBuilder);
+            Builder.Vehicle motorcycle3 = director.Construct(motorcycleBuilder);
             Console.WriteLine(motorcycle3.ToString());
 
             //object constructing using a fluent api implentation.  The director and the individual builders are replaced with a "fluent interface".
-            FluentBuilder.IVehicleBuilder secondBuilder = new FluentBuilder.VehicleBuilder();
-            FluentBuilder.Vehicle secondCar = secondBuilder
+            FluentBuilder.IVehicleBuilder fluentBuilder = new FluentBuilder.VehicleBuilder();
+            FluentBuilder.Vehicle car2 = fluentBuilder
                 .CreateVehicle("Sports Car")
                 .OnFrame("unibody")
                 .WithEngine(16, 9000)
                 .WithWheels(4)
                 .WithDoors(2)
                 .Construct();
-            Console.WriteLine(secondCar.ToString());
+            Console.WriteLine(car2.ToString());
 
             //using an implicit operator conversion...
-            FluentBuilder.Vehicle motorcycle4 = (FluentBuilder.VehicleBuilder)secondBuilder
+            FluentBuilder.Vehicle motorcycle4 = (FluentBuilder.VehicleBuilder)fluentBuilder
                 .CreateVehicle("Motorcycle")
                 .OnFrame("motorcycle frame")
                 .WithEngine(4, 250)
